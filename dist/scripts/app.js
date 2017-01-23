3bcc480a47582b9e0593e885501d9273b38dac3f
Josh Van Wyk <joshvanwyk@ymail.com>
2017-01-23 21:15:08 +0000

    Checkpoint 3 - Routing and States: completed

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,2 +1,23 @@
+(function() {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+            });
+            
+        $stateProvider
+            .state('landing', {
+                url: '/',
+                templateUrl: '/templates/landing.html'
+            })
+            .state('album', {
+                url: '/album',
+                templateUrl: '/templates/album.html'
+            });
+    }
+
 angular
-    .module('blocJams', []);
+    .module('blocJams', ['ui.router'])
+    .config(config);
+})();
